188f6cce757fdb2bea73d67988b487fdafa4f39c
Gerard <gerardherrera75@gmail.com>
2025-06-14 00:04:31 +0800

    Added Burger Menu Icon for small screen size

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import "./navbar.scss"
 
+
 function Navbar() {
+  const[open, setOpen] = useState(false);
   return (
     <nav>
         <div className="left">
@@ -14,8 +17,19 @@ function Navbar() {
           <a href="">Agents</a>
         </div>
         <div className="right">
-          <a href="">Sign in</a>
-          <a href="" className="register">Sign up</a>
+          <a href="/">Sign in</a>
+          <a href="/" className="register">Sign up</a>
+          <div className="menuIcon">
+            <img src="/menu.png" alt="" onClick={()=> setOpen((prev) => !prev)}/>
+          </div>
+          <div className={open ? "menu active" : "menu"}>
+            <a href="/">Home</a>
+            <a href="/">About</a>
+            <a href="/">Contacts</a>
+            <a href="/">Agents</a>
+            <a href="/">Sign in</a>
+            <a href="/">Sign up</a>
+          </div>
         </div>
     </nav>
   )
